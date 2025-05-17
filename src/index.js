7dae2bf1403ecaea13b9cbde92da59f0eb1b5112
ankhod <ankhod7@gmail.com>
2025-05-17 14:24:09 +0300

    1 changed index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import express from 'express';
+// src/index.js
 
-const app = express();
+import express from 'express';
 
 const PORT = 3000;
 
-app.get('/', (rec, res) => {
+const app = express();
+
+// Middleware для логування часу запиту
+app.use((req, res, next) => {
+  console.log(`Time: ${new Date().toLocaleString()}`);
+  next();
+});
+
+// Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
+// наприклад, у запитах POST або PATCH
+app.use(express.json());
+
+// Маршрут для обробки GET-запитів на '/'
+app.get('/', (req, res) => {
   res.json({
-    message: 'Hello world!',
+    message: 'Hello, World!',
+  });
+});
+
+// Middleware для обробких помилок (приймає 4 аргументи)
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: 'Something went wrong',
   });
 });
 
